6a6f33a0183d2643cefc9963d59ff61e7cc5c25d
Prateek Dubey <112685450+ishantdei@users.noreply.github.com>
2022-11-29 14:04:22 +0530

    Updated

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -286,7 +286,7 @@ function funst7(){
     document.getElementById("png7").style.visibility="hidden";
     document.getElementById("trans11").style.visibility="hidden";
     document.getElementById("arr11").style.visibility="hidden";
-    myTimeout9 = setTimeout(myGreeting9, 17000);
+    myTimeout9 = setTimeout(myGreeting9, 22000);
 
 
 }
@@ -357,4 +357,34 @@ function myGreeting11()
   }
 function myStopFunction() {
     clearTimeout(myTimeout11);  
+}
+
+// Step10
+
+function step10(){
+    document.getElementById("Step10").style.visibility = "visible";
+    document.getElementById("Step9").style.visibility="hidden";
+    document.getElementById("next8").style.visibility="hidden";
+    document.getElementById("gif12").style.visibility="hidden";
+    document.getElementById("png9").style.visibility="hidden";
+    document.getElementById("trans13").style.visibility="hidden";
+   
+}
+
+function funst10(){
+    document.getElementById("gif13").style.visibility="visible";
+    document.getElementById("png10").style.visibility="hidden";
+    document.getElementById("trans14").style.visibility="hidden";
+    document.getElementById("arr14").style.visibility="hidden";
+    myTimeout12 = setTimeout(myGreeting12, 25000);
+
+
+}
+function myGreeting12()
+     {
+    document.getElementById("next9").style.visibility="visible";
+    myStopFunction();
+  }
+function myStopFunction() {
+    clearTimeout(myTimeout12);  
 }
